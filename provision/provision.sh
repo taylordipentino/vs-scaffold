373a2bbecf84560addcb5eb51e32e6a74c1373b7
Taylor DiPentino <taylor.dipentino@gmail.com>
2019-03-03 02:47:27 -0500

    Added provisioning for Laravel

--- provision/provision.sh
+++ provision/provision.sh
@@ -6,7 +6,11 @@ project_name="scaffold"
 
 # The type of project to be set up
 # Possible values are wordpress, laravel
-project_type="wordpress"
+project_type="laravel"
+
+# The type of database to be set up
+# Possible values are mysql, none
+database_type="mysql"
 
 # Function to print messages along the way
 msg() {
@@ -20,13 +24,35 @@ msg "Updating the repositories and upgrading packages..."
 apt update
 apt upgrade -y
 
+# Determine what packages need to be installed
+msg "Determining packages to be installed based on project type"
+install_packages=(
+  apache2
+  php
+  libapache2-mod-php
+)
+if [ $database_type = "mysql" ]; then
+  install_packages+=('mysql-server')
+  install_packages+=('php-mysql')
+fi
+if [ $project_type = "laravel" ]; then
+  install_packages+=('php-tokenizer')
+  install_packages+=('php-mbstring')
+  install_packages+=('php-bcmath')
+  install_packages+=('php-json')
+  install_packages+=('php-xml')
+fi
+
 # Install required packages
 msg "Installing the required packages..."
-apt install -y apache2 mysql-server php php-mysql libapache2-mod-php
+if ! apt install -y ${install_packages[@]}; then
+  msg "ERROR: Failed to install required packages!"
+  exit 1
+fi
 
-# If applicable, set up the database to be used by Wordpress
-if [ $project_type = "wordpress" ]; then
-  msg "Setting up a database for Wordpress..."
+# If applicable, set up a MySQL database
+if [ $database_type = "mysql" ]; then
+  msg "Setting up a MySQL database..."
 
   # Generate the SQL to be executed
   sql="CREATE DATABASE $project_name; CREATE USER '$project_name'@'localhost' IDENTIFIED BY '$project_name'; GRANT ALL ON $project_name.* TO '$project_name'@'localhost'; FLUSH PRIVILEGES;"
@@ -47,6 +73,12 @@ fi
 msg "Changing the user and group that Apache runs as..."
 sed -i 's/www-data/vagrant/g' /etc/apache2/envvars
 
+if [ $project_type = "laravel" ]; then
+  # Change the document root
+  msg "Changing the document root..."
+  sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/public/' /etc/apache2/sites-available/000-default.conf
+fi
+
 # Change some PHP settings
 msg "Changing some PHP settings..."
 sed -i 's/memory_limit = 128M/memory_limit = 768M/' /etc/php/7.2/apache2/php.ini
@@ -55,7 +87,7 @@ sed -i 's/display_errors = Off/display_errors = On/' /etc/php/7.2/apache2/php.in
 
 # Make the document root a symlink to the Vagrant shared folder
 if ! [ -L /var/www/html ]; then
-  msg "Making the document root a symbolic root to the Vagrant shared folder..."
+  msg "Making the document root a symbolic link to the Vagrant shared folder..."
   rm -rf /var/www/html
   ln -fs /vagrant /var/www/html
 fi
@@ -64,6 +96,24 @@ fi
 msg "Enabling URL rewrites and restarting Apache..."
 a2enmod rewrite
 systemctl restart apache2
+
+# If necessary, install Composer
+if [ $project_type = "laravel" ]; then 
+  msg "Installing Composer..."
+  EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+  if [ $EXPECTED_SIGNATURE != $ACTUAL_SIGNATURE ]; then
+    msg "ERROR: Invalid installer signature"
+    rm composer-setup.php
+    exit 1
+  fi
+
+  php composer-setup.php --quiet
+  rm composer-setup.php
+  mv composer.phar /usr/local/bin/composer.phar
+fi
 
 # If applicable, get the latest version of Wordpress
 if [ $project_type = "wordpress" ]; then
@@ -79,4 +129,17 @@ if [ $project_type = "wordpress" ]; then
   cd ~/
 fi
 
+# If applicable, get the latest version of Laravel
+if [ $project_type = "laravel" ]; then
+  cd /var/www/html
+  composer.phar create-project --quiet --prefer-dist laravel/laravel laravel
+  mv laravel/* .
+  mv laravel/.* .
+  rm -r laravel
+  php artisan key:generate
+  cd ~/
+fi
+
 msg "All done! :)"
+
+
